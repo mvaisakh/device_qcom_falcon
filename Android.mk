e8975847e6bb53d4dfd2c4c9ad831da014fa8fa5
Rashed Abdel-Tawab <rashed@linux.com>
2017-02-24 22:43:52 -0500

    sdm660: Add IMS symlinks
    Change-Id: I35a5cd5e2327f82de44fc96934ca5457f28e0afc
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,3 +2,21 @@
 # This empty Android.mk file exists to prevent the build system from
 # automatically including any other Android.mk files under this directory.
 #
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),X01BD)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+$(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "IMS lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
+
+endif
